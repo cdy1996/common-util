36966a93d6c44d6ee2ebb4372371bb6dc6dff3d3
cdy1996 <13867868445@163.com>
2018-05-17 21:36:57 +0800

    string和collection的工具类

--- src/main/java/com/cdy/CollectionUtil.java
+++ src/main/java/com/cdy/CollectionUtil.java
@@ -1,9 +1,28 @@
 package com.cdy;
 
+import org.apache.commons.collections.CollectionUtils;
+
+import java.util.*;
+
 /**
  * todo
  * Created by 陈东一
  * 2018/5/15 22:33
  */
 public class CollectionUtil {
+
+    public static Boolean isEmpty(Collection collection){
+        return CollectionUtils.isEmpty(collection);
+    }
+    
+    public static Boolean isNotEmpty(Collection collection){
+        return CollectionUtils.isNotEmpty(collection);
+    }
+    
+    @SafeVarargs
+    public static <T> List<T> of(T... t){
+        return new ArrayList<T>(Arrays.asList(t));
+    }
+    
+    
 }
--- src/main/java/com/cdy/StringUtil.java
+++ src/main/java/com/cdy/StringUtil.java
@@ -1,9 +1,27 @@
 package com.cdy;
 
+import org.apache.commons.lang.StringUtils;
+
 /**
  * todo
  * Created by 陈东一
  * 2018/5/15 22:31
  */
 public class StringUtil {
+    
+    public static Boolean isEmpty(String string){
+        return StringUtils.isEmpty(string);
+    }
+    
+    public static Boolean isNotEmpty(String string){
+        return StringUtils.isNotEmpty(string);
+    }
+    
+    public static Boolean isBlank(String string){
+        return StringUtils.isBlank(string);
+    }
+    
+    public static Boolean isNotBlank(String string){
+        return StringUtils.isNotBlank(string);
+    }
 }
