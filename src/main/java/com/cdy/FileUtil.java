df98f9b90cb8d8de928713b17206818b8d43f3a6
cdy1996 <13867868445@163.com>
2018-05-24 21:46:59 +0800

    修改file和number工具类

--- src/main/java/com/cdy/FileUtil.java
+++ src/main/java/com/cdy/FileUtil.java
@@ -1,9 +1,22 @@
 package com.cdy;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+
 /**
  * todo
  * Created by 陈东一
  * 2018/5/15 22:32
  */
 public class FileUtil {
+    
+    public static void toFile(InputStream inputStream, String path) throws IOException {
+        FileUtils.copyInputStreamToFile(inputStream, new File(path));
+        
+    }
+    
+    public static FileInputStream openFile(File file) throws IOException {
+        return FileUtils.openInputStream(file);
+    }
 }
--- src/main/java/com/cdy/NumberUtil.java
+++ src/main/java/com/cdy/NumberUtil.java
@@ -1,5 +1,6 @@
 package com.cdy;
 
+import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 /**
@@ -15,6 +16,22 @@ public class NumberUtil {
     
     public static String format(String num, String pattern){
         return new DecimalFormat(pattern).format(num);
+    }
+    
+    public static BigDecimal format(BigDecimal bigDecimal){
+        return format(bigDecimal, 2);
+    }
+    
+    public static BigDecimal format(BigDecimal bigDecimal, int scale){
+        return bigDecimal.setScale(scale, BigDecimal.ROUND_HALF_UP);
+    }
+    
+    public static Double format(Double d){
+        return format(new BigDecimal(d)).doubleValue();
+    }
+    
+    public static Double format(Double d, int scale){
+        return format(new BigDecimal(d), scale).doubleValue();
     }
     
 }
