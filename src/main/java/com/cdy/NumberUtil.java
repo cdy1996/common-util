5342b229f6f158430d805e560b07a05dd3eddf4e
cdy1996 <13867868445@163.com>
2018-05-28 21:36:05 +0800

    修改注释

--- src/main/java/com/cdy/NumberUtil.java
+++ src/main/java/com/cdy/NumberUtil.java
@@ -4,32 +4,65 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 /**
- * todo
+ * 数字工具类
  * Created by 陈东一
  * 2018/5/23 21:21
  */
 public class NumberUtil {
-
+    
+    /**
+     * 数字格式化 保留2为小鼠
+     * @param num String
+     * @return String
+     */
     public static String format(String num){
         return format(num, "0.00");
     }
     
+    /**
+     * 数字格式化
+     * @param num String
+     * @param pattern String
+     * @return String
+     */
     public static String format(String num, String pattern){
         return new DecimalFormat(pattern).format(num);
     }
     
+    /**
+     * 数字格式化 保留两位  四舍五入
+     * @param bigDecimal BigDecimal
+     * @return BigDecimal
+     */
     public static BigDecimal format(BigDecimal bigDecimal){
         return format(bigDecimal, 2);
     }
     
+    /**
+     * 数字格式化  四舍五入
+     * @param bigDecimal BigDecimal
+     * @param scale int  保留位数
+     * @return BigDecimal
+     */
     public static BigDecimal format(BigDecimal bigDecimal, int scale){
         return bigDecimal.setScale(scale, BigDecimal.ROUND_HALF_UP);
     }
     
+    /**
+     * 数字格式化 保留两位
+     * @param d Double
+     * @return BigDecimal
+     */
     public static Double format(Double d){
         return format(new BigDecimal(d)).doubleValue();
     }
     
+    /**
+     * 数字格式化
+     * @param d Double
+     * @param scale int 保留位数
+     * @return BigDecimal
+     */
     public static Double format(Double d, int scale){
         return format(new BigDecimal(d), scale).doubleValue();
     }
--- src/main/java/com/cdy/StringUtil.java
+++ src/main/java/com/cdy/StringUtil.java
@@ -6,33 +6,59 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * todo
+ * 字符串工具类
  * Created by 陈东一
  * 2018/5/15 22:31
  */
 public class StringUtil {
     
-    public static Boolean isEmpty(String string){
+    /**
+     * 判断是否为空
+     * @param string String
+     * @return Boolean
+     */
+    public static boolean isEmpty(String string){
         return StringUtils.isEmpty(string);
     }
     
-    public static Boolean isNotEmpty(String string){
+    /**
+     * 判断是否不为空
+     * @param string String
+     * @return Boolean
+     */
+    public static boolean isNotEmpty(String string){
         return StringUtils.isNotEmpty(string);
     }
     
-    public static Boolean isBlank(String string){
+    /**
+     * 判断是否为空白
+     * @param string String
+     * @return boolean
+     */
+    public static boolean isBlank(String string){
         return StringUtils.isBlank(string);
     }
     
-    public static Boolean isNotBlank(String string){
+    /**
+     * 判断是否为不空白
+     * @param string String
+     * @return boolean
+     */
+    public static boolean isNotBlank(String string){
         return StringUtils.isNotBlank(string);
     }
     
-    public static Boolean contain(String str, String regex){
+    /**
+     * 正则检查是否匹配
+     * @param string String
+     * @param regex String
+     * @return boolean
+     */
+    public static boolean match(String string, String regex){
         Pattern pattern = Pattern.compile(regex);
         // 忽略大小写的写法
         // Pattern pat = Pattern.compile(regEx, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(str);
+        Matcher matcher = pattern.matcher(string);
         // 字符串是否与正则表达式相匹配
         return matcher.matches();
     }
